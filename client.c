5de3a9a0d8ca3c4bb9ce7fd894862546f4cb7aeb
Taha_Helmy <153909093+Thelmi@users.noreply.github.com>
2024-03-29 15:40:56 +0400

    still under construction

--- client.c
+++ client.c
@@ -1,65 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thelmy <thelmy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 14:44:20 by thelmy            #+#    #+#             */
+/*   Updated: 2024/03/29 15:17:51 by thelmy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <string.h>
+#include "minitalk.h"
 
-// volatile int t_signal = 0;
-void res_handler(int sig)
+static int	ft_atoi(char *str)
 {
-	if(sig)
+	int	i;
+	int	sign;
+	int	result;
+	
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
 	{
-		write(1, "recieved\n",strlen("recieved\n"));
-		
-		}
-}
-// void sigusr_handler(int sig, siginfo_t *info, void *context)
-// {
-// 	(void)context;
-
-// 	if(sig == SIGUSR1 && !t_signal)
-// 		write(1, "OK ooooooo", 11);
-// 	else
-// 		t_signal ++;
-
-// }
-void send_signal(int pid, char c)
-{
-	int i = 8;
-
-	while(i--)
-	{
-		if (c & 1 << i) 
-		{
-			kill(pid,SIGUSR2);// printf("true\n");
-		}
-		else
-		{
-			kill(pid,SIGUSR1);
-			// printf("false\n");
-		}
-			usleep(350);
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
-	// printf("%c\n", c);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * result);
 }
 
-int main(int ac, char **av)
+static void	res_handler(int sig)
 {
-	int i = 0;
-	struct sigaction sa1;
+	if (sig)
+		write (1, "recieved\n",10);
+}
+
+static void	send_signal(int pid, char c)
+{
+	int	i;
+
+	i = 8;
+	while (i--)
+	{
+		if (c & 1 << i)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(350);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int					i;
+	struct sigaction	sa1;
+
+	i = 0;
 	sa1.sa_handler = &res_handler;
 	sigaction(SIGUSR1, &sa1, NULL);
+	if (ft_atoi(av[1]) < 0 || ft_atoi(av) > )
+		write(1, "\n", 17);
 	if (ac == 3)
 	{
-		while(av[2][i])
+		while (av[2][i])
 		{
-			send_signal(atoi(av[1]),av[2][i]);
+			send_signal(atoi(av[1]), av[2][i]);
 			i++;
 		}
-		send_signal(atoi(av[1]),'\0');
+		send_signal (atoi(av[1]), '\0');
 	}
+	write(1, "not 2 arguements\n", 18);
 }
-
-
-
--- server.c
+++ server.c
@@ -5,68 +5,59 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thelmy <thelmy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/20 17:22:24 by thelmy            #+#    #+#             */
-/*   Updated: 2024/03/22 22:31:40 by thelmy           ###   ########.fr       */
+/*   Created: 2024/03/29 14:45:03 by thelmy            #+#    #+#             */
+/*   Updated: 2024/03/29 15:15:27 by thelmy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
+#include "minitalk.h"
 
-void sigusr_handler(int sig, siginfo_t *info, void *context)
+void	sigusr_handler(int sig, siginfo_t *info, void *context)
 {
-	static int i;
-	static int c; 
-	// if(sig == SIGUSR2)
-	// 	write(1,"true\n", 5);
-	// if(sig == SIGUSR1)
-	// 	write(1,"false\n",6);
+	static int	i;
+	static int	c;
+	char		cp;
+
 	if (!i)
 	{
 		i = 8;
 		c = 0;
 	}
-
 	if (sig == SIGUSR2)
 		c = c | 1 << (i - 1);
-	// else if (sig == SIGUSR1)
-	// 	c = c | 0;
 	if (i == 1)
 	{
-		// printf("(%c\n)", c);
-		// fflush(NULL);
-		char cp = c + 0;
+		cp = c + 0;
 		write(1, &cp, 1);
-		if(c == '\0')
-			kill(info -> si_pid,SIGUSR1);
+		if (c == '\0')
+			kill(info -> si_pid, SIGUSR1);
 	}
-	i--; 
-	
-	
+	i--;
 }
 
-void putnbr(int n)
+void	ft_putnbr(int n)
 {
-	if(n > 9)
+	char	digit;
+
+	if (n > 9)
 		putnbr(n / 10);
-	write(1, &(int){'0' + n % 10}, 1); 
+	digit = (n % 10) + '0';
+	write(1, &digit, 1);
 }
-int main()
+
+int	main(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
+	pid_t				pid;
+
 	sa.sa_sigaction = &sigusr_handler;
 	sa.sa_flags = SA_SIGINFO;
-	pid_t pid = getpid();
-	putnbr(pid);
-	sigaction(SIGUSR1,&sa, NULL);
-	sigaction(SIGUSR2,&sa, NULL);
-	while(1)
+	pid = getpid();
+	ft_putnbr(pid);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
 	{
-	// signal(SIGUSR1, sigusr_handler);
-	// signal(SIGUSR2, sigusr_handler);
-	
-	pause();
+		pause();
 	}
 }
